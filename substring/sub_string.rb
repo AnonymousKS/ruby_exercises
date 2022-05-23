e535c25c12b69000050b6f50c6927e56a49c75c5
AnonymousKS <kunal26june@gmail.com>
2022-05-23 20:18:41 +0530

    works for both the cases now

--- substring/sub_string.rb
+++ substring/sub_string.rb
@@ -1,11 +1,11 @@
 def substrings(str, dictionary)
   str.downcase!
+  total_count = 0
   new_hash = {}
   dictionary.each do |word|
-    flag = 0
-    if str.include?(word)
-      flag = flag + 1
-      new_hash[word] = flag
+    count_total = str.scan(word).length
+    if count_total != 0
+      new_hash[word] = count_total
     end
   end
   return new_hash
@@ -13,4 +13,4 @@ end
 
 dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
 
-puts substrings("below", dictionary)
+puts substrings("Howdy partner, sit down! How's it going?", dictionary)
